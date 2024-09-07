e8f073183cb05a2b335acd8116334ce071914330
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2024-09-07 16:02:10 +0530

    first commit

--- src/Employee.java
+++ src/Employee.java
@@ -15,5 +15,23 @@ public class Employee {
         emp.EmployeeName = "John Doe";                                    /* Same as such follows for every variable that has been initialized and assigned */
         emp.EmployeeAddress = "123 Main St";
         emp.display();                                                    /* Calling the display method to display all the values passed to the 'emp' object */
+
+
+        Employee emp2 = new Employee();
+        emp2.EmployeeID = 2;
+        emp2.EmployeeSalary = 2000;
+        emp2.EmployeeName = "John Doe";
+        emp2.EmployeeAddress = "123 Main St";
+        System.out.println("Employee ID: " + emp.EmployeeID);
+        emp.display();                                                   /* The output for the total code is
+                                                                            Employee ID: 1
+                                                                            Employee Salary: 1000
+                                                                            Employee Name: John Doe
+                                                                            Employee Address: 123 Main St
+                                                                            Employee ID: 1
+                                                                            Employee ID: 1
+                                                                            Employee Salary: 1000
+                                                                            Employee Name: John Doe
+                                                                            Employee Address: 123 Main St  */
     }
 }
