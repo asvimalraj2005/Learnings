076dd8a831de02ab386d34b9764aa5e18bec9d18
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2024-09-07 15:45:30 +0530

    first commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,20 @@
 public class Employee {
+    int EmployeeID;
+    int EmployeeSalary;
+    String EmployeeName;
+    String EmployeeAddress;
+    void display(){
+        System.out.println("Employee ID: " + EmployeeID);
+        System.out.println("Employee Salary: " + EmployeeSalary);
+        System.out.println("Employee Name: " + EmployeeName);
+        System.out.println("Employee Address: " + EmployeeAddress);
+    }
+    public static void main(String[] args){
+        Employee emp = new Employee();
+        emp.EmployeeID = 1;
+        emp.EmployeeSalary = 1000;
+        emp.EmployeeName = "John Doe";
+        emp.EmployeeAddress = "123 Main St";
+        emp.display();
+    }
 }
